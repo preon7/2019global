4ed3c4023b46a1e9c3f4ac9bb6c3f81d9dd38fb5
ruihengwu <preon7@gmail.com>
2019-11-17 22:12:14 +0100

    implement sphere

--- include/entities.h
+++ include/entities.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <math.h>
 
 #include "bbox.h"
 #include "material.h"
 #include "ray.h"
+
+#include <iostream>
+#include <algorithm>
 
 /// A base class for all entities in the scene.
 struct Entity {
@@ -24,6 +28,72 @@ struct Entity {
 };
 
 // TODO Implement implicit sphere
+class ImpSphere : public Entity {
+public:
+    ImpSphere(glm::dvec3 pos, float radius) : Entity(), radius(radius) {
+        this->pos = pos;
+    }
+    
+    float radius;
+    
+    // (x-p)^2 + (y-p)^2 + (z-p)^2 = r^2
+    
+    bool intersect(const Ray& ray, glm::dvec3& intersect, glm::dvec3& normal) const {
+        // shift the position
+        glm::dvec3 new_pos = this->pos - ray.origin;
+        
+        float a_1 = 1;
+        float a_2 = 1;
+        float a_3 = 1;
+        if (ray.dir.x != 0) {
+            a_2 = ray.dir.y / ray.dir.x;
+            a_3 = ray.dir.z / ray.dir.x;
+        } else if (ray.dir.y != 0) {
+            a_1 = ray.dir.x / ray.dir.y;
+            a_3 = ray.dir.z / ray.dir.y;
+        } else if (ray.dir.z != 0) {
+            a_2 = ray.dir.y / ray.dir.z;
+            a_1 = ray.dir.x / ray.dir.z;
+        } else { return false; }
+        
+        float a = pow(a_1, 2) + pow(a_2, 2) + pow(a_3, 2);
+        float b = -2 * (new_pos.x * a_1 + new_pos.y * a_2 + new_pos.z * a_3);
+        float c = pow(new_pos.x, 2) + pow(new_pos.y, 2) + pow(new_pos.z, 2) - pow(radius, 2);
+        
+        if (pow(b, 2) - 4 * a * c < 0) {
+//            std::cout << a << std::endl;
+//            std::cout << b << std::endl;
+//            std::cout << c << std::endl;
+            return false;
+        } else {
+            float v_1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+            float v_2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+            
+            // use the nearest one
+            float base = std::min(v_1, v_2);
+            if (a_1 == 1) {
+                intersect = glm::dvec3{base, base * a_2, base * a_3};
+            } else if (a_2 == 1) {
+                intersect = glm::dvec3{base * a_1, base, base * a_3};
+            } else {
+                intersect = glm::dvec3{base * a_1, base * a_2, base};
+            }
+            
+            // shift intersection point back
+            intersect = intersect + ray.origin;
+            normal = glm::normalize(intersect - this->pos);
+            
+            return true;
+        };
+        
+        return false;
+    }
+    
+    BoundingBox boundingBox() const {
+        BoundingBox b = BoundingBox(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(1.0f,1.0f,1.0f));
+        return b;
+    }
+};
 // TODO Implement implicit triangle
 
 // TODO Implement explicit sphere (triangles)
--- main.cpp
+++ main.cpp
@@ -5,8 +5,16 @@
 #include "camera.h"
 #include "gui.h"
 
+// for test
+#include "ray.h"
+#include "entities.h"
+#include "glm/ext.hpp"
+
+void test();
+
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
+    test();
 
     Camera camera({10, 0, 0});
     glm::dvec3 light{10, 10, 10};
@@ -23,4 +31,20 @@ int main(int argc, char** argv) {
     Gui window(500, 500, raytracer);
     window.show();
     return app.exec();
+}
+
+void test() {
+    ImpSphere s = ImpSphere(glm::dvec3{2,0,0}, 2);
+    std::cout << 2 / 0 << std::endl;
+    std::cout << "sphere created\n";
+    std::cout << "sphere radius: " << s.radius << std::endl;
+    std::cout << "sphere position: " << glm::to_string(s.pos) << std::endl;
+    
+    Ray r = Ray(glm::dvec3{0,0,0}, glm::dvec3{0,1,0});
+    glm::dvec3 intersect = glm::dvec3{0,0,0};
+    glm::dvec3 normal = glm::dvec3{0,0,0};
+    
+    std::cout << "if intersection: " << s.intersect(r, intersect, normal) << std::endl;
+    std::cout << "intersection point: " << glm::to_string(intersect) << std::endl;
+    std::cout << "intersection normal: " << glm::to_string(normal) << std::endl;
 }
