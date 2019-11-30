611ef9e837ab6b7d40d8dce582d006363ec6204b
ruihengwu <preon7@gmail.com>
2019-11-30 21:34:59 +0100

    change parameters

--- include/material.h
+++ include/material.h
@@ -10,20 +10,38 @@
 /// Represents the material properties of an entity. For now it only contains color, but it should
 /// probably be extended to allow more options.
 struct Material {
-    constexpr explicit Material(glm::dvec3 color) : color(std::move(color)) {}
+    constexpr explicit Material(glm::dvec3 color) : color(std::move(color)) {
+        specular_color = {1,1,1};
+        diffuse_color = color * 0.5;
+    }
+    
+    Material(glm::dvec3 color, glm::dvec3 shader) : color(std::move(color)), shader_parameters(shader) {
+        specular_color = {1,1,1};
+        diffuse_color = color * 0.5;
+    }
 
     glm::dvec3 color;
-    glm::dvec3 shader_parameters = glm::dvec3(0.05,0.3,0.4);
+    glm::dvec3 diffuse_color;
+    glm::dvec3 specular_color;
+    
+    glm::dvec3 shader_parameters = glm::dvec3(0.1,0.7,1);
     
     glm::dvec3 blinn_phong(Ray ray, glm::dvec3 light, glm::dvec3 intersect, glm::dvec3 normal) {
-        double specular_power = 1;
+        double specular_power = 5;
         
         glm::dvec3 la = color * shader_parameters.x;
-        glm::dvec3 ld = std::max(0.0, glm::dot(normal, glm::normalize(light - intersect))) * color * shader_parameters.y;
+        glm::dvec3 ld = std::max(0.0, glm::dot(normal, glm::normalize(light - intersect))) * diffuse_color * shader_parameters.y;
         glm::dvec3 bisector = glm::normalize(glm::normalize(-ray.dir) + glm::normalize(light - intersect));
-        glm::dvec3 ls = pow(std::max(0.0, glm::dot(normal, bisector)), specular_power) * color * shader_parameters.z;
-        double inter = std::max(0.0, glm::dot(normal, bisector));
+        glm::dvec3 ls = pow(std::max(0.0, glm::dot(normal, bisector)), specular_power) * specular_color * shader_parameters.z;
+       
+        auto output = la + ld + ls;
         
-        return la + ld + ls;
+//        double max_val = std::max(std::max(output.x, output.y), output.z);
+//        if (max_val > 1.0) { output = output / max_val; }
+        
+        // prevent the value from exceeding 1
+        output = {std::min(output.x, 1.0), std::min(output.y, 1.0), std::min(output.z, 1.0)};
+        
+        return output;
     }
 };
